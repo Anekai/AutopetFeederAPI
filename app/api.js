b051d5cfc9e3b3b03a8c3e73f3964157a8a39dfd
Anekai <36900069+Anekai@users.noreply.github.com>
2021-10-28 20:38:26 -0300

    Mudança na porta do servidor

--- app/api.js
+++ app/api.js
@@ -2,8 +2,8 @@ require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const app = express();
 
-app.listen(3300, ()=>{
-    console.log("Server is now listening at port 3300");
+app.listen(3333, ()=>{
+    console.log("Server is now listening at port 3333");
 });
 
 app.use(express.json());
